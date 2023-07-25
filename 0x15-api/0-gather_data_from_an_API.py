3c043fcb7c195e07433716a889d5a3f1d967c415
aloogladys <aloogladys@gmail.com>
2023-07-25 13:10:13 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Write a script that hits a REST api that has data on employees and tasks
+and filters the data based on an argument passed to the script.
+The argument is the employee ID, and the output should display the employee
+name and the tasks the employee completed.
+"""
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    eid = sys.argv[1]
+    name = requests.get("http://jsonplaceholder.typicode.com/users/{}"
+                        .format(eid)).json().get("name")
+    total_tasks = 0
+    done_tasks = []
+    r = requests.get("http://jsonplaceholder.typicode.com/todos").json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    for task in r:
+        if (task.get("userId") == int(eid)):
+            total_tasks += 1
+            if (task.get("completed")):
+                done_tasks.append(task.get("title"))
+
+    print("Employee {} is done with tasks({:d}/{:d}):"
+          .format(name, len(done_tasks), total_tasks))
+
+    for item in done_tasks:
+        print("\t {}".format(item))
